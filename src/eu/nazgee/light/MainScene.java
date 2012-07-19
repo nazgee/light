a0a2f6d804cb8e84bf1580c696fc088a58ef4a4d
Michal Stawinski <michal.stawinski@gmail.com>
2012-07-20 01:38:47 +0200

    OffscreenSprite

--- src/eu/nazgee/light/MainScene.java
+++ src/eu/nazgee/light/MainScene.java
@@ -16,6 +16,8 @@ import org.andengine.opengl.util.GLState;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.util.adt.color.Color;
 
+import eu.nazgee.utils.OffscreenSprite;
+
 public class MainScene extends Scene{
 	// ===========================================================
 	// Constants
@@ -34,39 +36,27 @@ public class MainScene extends Scene{
 		super();
 		mRenderTexture = pTexturesLibrary.getRenderTexture();
 	
-		// Prepare Sprite hierarchy that will be renderred offscreen to the Texture
-		Sprite offscreenSun = new Sprite(mRenderTexture.getWidth()/2, mRenderTexture.getHeight()/2, pTexturesLibrary.getSun(), pVertexBufferObject) {
-			@Override
-			protected void onManagedDraw(GLState pGLState, Camera pCamera) {
-				if (!mRenderTexture.isInitialized()) {
-					mRenderTexture.init(pGLState);
-				}
+		// Prepare reference Sprite hierarchy, rendered in a traditional way 
+		Sprite referenceHierarchy = new Sprite(500, 200, pTexturesLibrary.getSun(), pVertexBufferObject);
+		attachReferencePlanets(referenceHierarchy, pTexturesLibrary, pVertexBufferObject);
+		attachChild(referenceHierarchy);
 
-				{
-					mRenderTexture.begin(pGLState, false, true, Color.TRANSPARENT);
-					super.onManagedDraw(pGLState, pCamera);
-					mRenderTexture.end(pGLState);
-				}
-			}
-		};
-		attachReferencePlanets(offscreenSun, pTexturesLibrary, pVertexBufferObject);
-		attachChild(offscreenSun);
+		// Prepare offscreen Sprite
+		OffscreenSprite offscreen = new OffscreenSprite(mRenderTexture.getWidth()/2, mRenderTexture.getHeight()/2, mRenderTexture, pTexturesLibrary.getSun(), pVertexBufferObject);
+		attachReferencePlanets(offscreen, pTexturesLibrary, pVertexBufferObject);
+		attachChild(offscreen);
 
-		// Prepare sprite which will display offscreen-generated hierarchy
+		// Prepare sprite which will display offscreen-generated stuff
 		Sprite resultSprite = new Sprite(200, 200, TextureRegionFactory.extractFromTexture(mRenderTexture), pVertexBufferObject);
 		attachChild(resultSprite);
 
-		// Prepare reference Sprite hierarchy, rendered in a traditional way 
-		Sprite referenceSun = populateReferenceHierarchy(pTexturesLibrary, pVertexBufferObject);
-		attachChild(referenceSun);
-
-		referenceSun.registerEntityModifier(new LoopEntityModifier(new RotationModifier(25, 0, 360)));
-		referenceSun.registerEntityModifier(new LoopEntityModifier(new SequenceEntityModifier(
+		referenceHierarchy.registerEntityModifier(new LoopEntityModifier(new RotationModifier(25, 0, 360)));
+		referenceHierarchy.registerEntityModifier(new LoopEntityModifier(new SequenceEntityModifier(
 				new MoveByModifier(5, 100, 0),
 				new MoveByModifier(5, -100, 0)
 				)));
 
-		offscreenSun.registerEntityModifier(new LoopEntityModifier(new RotationModifier(25, 0, 360)));
+		offscreen.registerEntityModifier(new LoopEntityModifier(new RotationModifier(25, 0, 360)));
 		resultSprite.registerEntityModifier(new LoopEntityModifier(new SequenceEntityModifier(
 				new MoveByModifier(5, 100, 0),
 				new MoveByModifier(5, -100, 0)
@@ -91,11 +81,6 @@ public class MainScene extends Scene{
 	// Getter & Setter
 	// ===========================================================
 
-	private Sprite populateReferenceHierarchy(TexturesLibrary pTexturesLibrary, VertexBufferObjectManager pVertexBufferObject) {
-		Sprite referenceSun = new Sprite(500, 200, pTexturesLibrary.getSun(), pVertexBufferObject);
-		attachReferencePlanets(referenceSun, pTexturesLibrary, pVertexBufferObject);
-		return referenceSun;
-	}
 	private void attachReferencePlanets(Sprite referenceSun, TexturesLibrary pTexturesLibrary, VertexBufferObjectManager pVertexBufferObject) {
 		Sprite planet1 = new Sprite(0, 0, pTexturesLibrary.getPlanet(0), pVertexBufferObject);
 		Sprite planet2 = new Sprite(200, 200, pTexturesLibrary.getPlanet(1), pVertexBufferObject);
